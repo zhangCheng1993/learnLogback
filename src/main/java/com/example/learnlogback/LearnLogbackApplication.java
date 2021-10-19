dbb6cf5e82e6f707db390b28f43d42ef2e2c5f16
zhangcheng24 <zhangcheng24@58.com>
2021-10-19 23:40:01 +0800

    Initial commit

---
+++ src/main/java/com/example/learnlogback/LearnLogbackApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnlogback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnLogbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnLogbackApplication.class, args);
+    }
+
+}
